837c405816ddba233e54b4364427e7ba2b2f1613
Porous <118331091+muttu70@users.noreply.github.com>
2024-04-03 12:22:01 +0530

    Update prog7pri.c

--- prog7pri.c
+++ prog7pri.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
 
 // Function to swap two variables
-void swap(int *a, int *b) {
+void swap(int *a, int *b)
+{
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-int main() {
+int main() 
+{
     int n;
     printf("Enter Number of Processes: ");
     scanf("%d", &n);
 
     // b is array for burst time, p for priority and index for process id
     int b[n], p[n], index[n];
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         printf("Enter Burst Time and Priority Value for Process %d: ", i + 1);
         scanf("%d %d", &b[i], &p[i]);
         index[i] = i + 1;
     }
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+        {
         int a = p[i], m = i;
 
         // Finding out the highest priority element and placing it at its desired position
-        for (int j = i; j < n; j++) {
-            if (p[j] > a) {
+        for (int j = i; j < n; j++) 
+        {
+            if (p[j] > a)
+            {
                 a = p[j];
                 m = j;
             }
@@ -42,7 +48,8 @@ int main() {
 
     // Printing scheduled process
     printf("Order of process Execution is\n");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         printf("P%d is executed from %d to %d\n", index[i], t, t + b[i]);
         t += b[i];
     }
@@ -50,7 +57,8 @@ int main() {
 
     printf("Process Id     Burst Time   Wait Time    TurnAround Time\n");
     int wait_time = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         printf("P%d          %d          %d          %d\n", index[i], b[i], wait_time, wait_time + b[i]);
         wait_time += b[i];
     }
